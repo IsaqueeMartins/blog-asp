264da67015f78b62c8e0c54ebf78190b8126d000
Isaque <isaque.martins.barros@gmail.com>
2024-08-29 22:46:25 -0300

    wip(apiURL): configurando funcoes de url para cada requisicao

--- src/services/index.js
+++ src/services/index.js
@@ -1,1 +1,18 @@
+class apiUrl {
+    baseUrl()
+    {
+        var baseUrl = 'https://localhost:7056/api/'
+        return baseUrl;
+    }
 
+    buscaArtigosUrl()
+    {
+        var getUrl = this.baseUrl.baseUrl + '/Artigo/'
+        return getUrl;
+    }
+
+    buscaArtigoIdUrl()
+    {
+        var getUrl = this.baseUrl.baseUrl + '/Artigo/' + 
+    }
+}
